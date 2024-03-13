66e4b4dbf93b97c8d106dd8390af4d2bcc093827
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-13 21:43:22 +0000

    add Searchbar for possible future implementation to navbar

--- moteify/settings.py
+++ moteify/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-cianthornhill-moteify-9xqp3rbpowy.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-cianthornhill-moteify-9xqp3rbpowy.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
