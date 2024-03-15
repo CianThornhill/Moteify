1dfdef9d1ff3911acb75ed37fddf86f3d7dc150b
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-15 17:39:42 +0000

    update EmoteForm forms.py to use Pillow to check image width and height to resolve error

--- emotes/forms.py
+++ emotes/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Emote
 from cloudinary.models import CloudinaryField
+from PIL import Image
 
 class EmoteForm(forms.ModelForm):
     """
@@ -13,7 +14,9 @@ class EmoteForm(forms.ModelForm):
     def clean_emote_img(self):
         emote_img = self.cleaned_data.get('emote_img')
         if emote_img:
-            width, height = emote_img.width, emote_img.height
+            # Open the image file using Pillow
+            image = Image.open(emote_img)
+            width, height = image.size
             if (width, height) not in ((512, 512), (1024, 1024)):
                 raise forms.ValidationError("Image must be either 512x512 or 1024x1024 pixels.")
         return emote_img
