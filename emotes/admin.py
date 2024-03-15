42e033f72b4c9062a1b6c20e2914f9a123671361
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-15 15:21:27 +0000

    add emote model to admin.py and check that it is working

--- emotes/admin.py
+++ emotes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Emote
 
 # Register your models here.
+
+@admin.register(Emote)
+class EmoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'get_emote_img_url',
+        'title',
+        'category',
+        'upload_date',
+    )
+
+    list_filter=('user','category',)
+
+    def get_emote_img_url(self, obj):
+        return obj.emote_img.url if obj.emote_img else None
+    get_emote_img_url.short_description = 'Emote Image URL'
