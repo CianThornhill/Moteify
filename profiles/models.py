47b276fc68f7e35ee16289bfd52e9625ba7aa44a
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-15 10:55:14 +0000

    Bugfix: add default_image field to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 from djrichtextfield.models import RichTextField
 
 from cloudinary.models import CloudinaryField
-from django_resized import ResizedImageField
 
 # Create your models here.
 class Profile(models.Model):
@@ -15,12 +12,20 @@ class Profile(models.Model):
     Model for User Profiles 
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
     profile_image = CloudinaryField(
-        default="/static/img/user_default.png",
+        folder = "user_images",
+        default = None,
         transformation=[
             {'width':512, 'height': 512, 'crop': 'fill'}
-        ]
+        ],
+        null=True
     )
+
+    default_image = models.ImageField(
+        default="/static/img/user_default.png" 
+    )
+
     display_name = models.CharField(max_length=25, blank=True, null=True)
     bio = RichTextField(max_length=2000, null=True, blank=True)
 
