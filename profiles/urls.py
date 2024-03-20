fea88d98262f9e301100a81ec0aae4a7f48cdf34
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-20 01:05:08 +0000

    add delete account functionality, edit paginate style for easier mobile use, footer styling + readability

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import profile_view
+from .views import profile_view, delete_account
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('', profile_view, name='profile_view'),
+    path('delete_account/', delete_account, name='delete_account'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import UserProfileForm
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
@@ -28,3 +29,14 @@ def profile_view(request):
 
     # Pass form to the template context
     return render(request, 'profile.html', {'form': form, 'user_profile': user_profile})
+
+@login_required
+def delete_account(request):
+    if request.method == 'POST':
+        # Delete the user account
+        request.user.delete()
+        # Log out the user
+        logout(request)
+        # Redirect to a thank you page or home page
+        return redirect('emotes')  # Replace 'home' with the URL name of your home page
+    return render(request, 'delete_account.html')
